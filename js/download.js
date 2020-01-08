930fd14ac049e8d3b5251185aca53401a30a0b5e
Mehdi NACIRI <mnaciri@enseirb-matmeca.fr>
2020-01-08 19:17:24 +0100

    Upgrade du site web en retirant les éléments redondants + petite modif de style sur la navbar

--- js/download.js
+++ js/download.js
@@ -1,5 +1,5 @@
 function addArticle() {
-	$("#articles").empty();
+	$("#articles").empty();/** 
 	let articleTemplate = $('#articleTemplate').html();
 	Mustache.parse(articleTemplate);
 	let articleRendered=Mustache.render(articleTemplate, {
@@ -8,7 +8,7 @@ function addArticle() {
 		Contenu:"Le but de notre projet est de faire communiquer en WIFI plusieurs véhicules autonomes afin de les faire circuler en convois. Pour cela, nous souhaitons utiliser le protocole 802.11p qui est adapté à la communication entre véhicules."
 	});
 	$("#articles").append(articleRendered);
-
+	*/
 	let endTemplate = $('#EndArticleTemplate').html();
 	Mustache.parse(endTemplate);
 	let endRendered=Mustache.render(endTemplate, {});
