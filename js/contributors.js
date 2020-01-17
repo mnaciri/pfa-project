e866ac99fee7a93898ea0ad5ae113b0993c1c857
Mehdi NACIRI <mnaciri@enseirb-matmeca.fr>
2020-01-17 15:08:40 +0100

    Ajout de photos pour l'onglet contributeurs

--- js/contributors.js
+++ js/contributors.js
@@ -1,10 +1,11 @@
-function addArticle(nom, prenom, mail) {
+function addArticle(nom, prenom, mail, img) {
 	let articleTemplate = $('#articleTemplate').html();
 	Mustache.parse(articleTemplate);
 	let articleRendered=Mustache.render(articleTemplate, {
 		Titre:nom,
 		Sstitre:prenom,
-		Contenu:mail
+		Contenu:mail,
+		image:img
 	});
 	$("#articles").append(articleRendered);
 }
@@ -29,8 +30,9 @@ $(document).ready(function() {
 	let noms=["CHIA", "GEYNET", "DISDIER", "MIRANDE", "JAFFRAIN", "CHANCOGNE", "NACIRI"];
 	let prenoms=["Aissam", "Raphaël", "Alexandre", "Julien", "Amaury", "Mael", "Mehdi"];
 	let mails=["aissam.chia@bordeaux-inp.fr", "rgeynet@enseirb-matmeca.fr", "alexandre.disdier@bordeaux-inp.fr", "julien.mirande@bordeaux-inp.fr", "amaury.jaffrain@bordeaux-inp.fr", "mael.chancogne@bordeaux-inp.fr", "mehdi.naciri@bordeaux-inp.fr"];
+	let images=["./img/aissam.jpg", "./img/avatar-homme.png", "./img/avatar-homme.png", "./img/avatar-homme.png", "./img/avatar-homme.png", "./img/mael.jpg", "./img/avatar-homme.png"]
 	for(let i=0; i<noms.length; i++) {
-		addArticle(noms[i], prenoms[i], mails[i]);
+		addArticle(noms[i], prenoms[i], mails[i], images[i]);
 		if (i<noms.length-1)
 			separateArticle();
 		else
